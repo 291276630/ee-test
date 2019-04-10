833e26d65a27ca024648c093c205faf739118e09
Asen <Asen0713@163.com>
2019-04-10 19:33:07 +0800

    Initial commit

---
+++ ee-upms/ee-upms-common/src/main/java/com/ee/upms/common/dao/UpmsUserMapper.java
@@ -0,0 +1,18 @@
+package com.ee.upms.common.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ee.upms.common.model.UpmsUser;
+
+public interface UpmsUserMapper extends BaseMapper<UpmsUser> {
+    /*int deleteByPrimaryKey(Integer id);
+
+    int insert(UpmsUser record);
+
+    int insertSelective(UpmsUser record);
+
+    UpmsUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UpmsUser record);
+
+    int updateByPrimaryKey(UpmsUser record);*/
+}
