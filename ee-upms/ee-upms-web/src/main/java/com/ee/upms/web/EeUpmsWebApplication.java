c478cc9e8f2c632df29f37eee9914b9d160e02ba
Asen <Asen0713@163.com>
2019-04-10 19:29:23 +0800

    Initial commit

--- ee-upms/ee-upms-web/src/main/java/com/ee/upms/web/EeUpmsWebApplication.java
+++ ee-upms/ee-upms-web/src/main/java/com/ee/upms/web/EeUpmsWebApplication.java
@@ -1,9 +1,11 @@
 package com.ee.upms.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ee.upms.common.dao.mapper")
 public class EeUpmsWebApplication {
 
 	public static void main(String[] args) {
